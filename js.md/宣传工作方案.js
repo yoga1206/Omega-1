e63e6585e4c6f77ef9ea745b81f29304bf83a227
Marmotlian <119547943@qq.com>
2017-05-26 00:37:23 +0800

    js跟markdown都在里面了

---
+++ js.md/宣传工作方案.js
@@ -0,0 +1,15 @@
+£û
+      Draft department:String, 
+      Draft Time:Date,   
+      Draft people:String,   
+      Telephonenumber:String,  
+      Title:String,    
+      Program sort:String,   
+      Principal:String, 
+      Member:String,   
+      Budget:Float,   
+      Actual money:Float,  
+      Run time1:Date,  
+      Run time2:Date,   
+      Remark:String  
+£ý
